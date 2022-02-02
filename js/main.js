83d3b84f19893b8070ca7f269ae3a86f02c9ce7a
Miłosz Krawczyk <mixol456@gmail.com>
2022-02-02 07:26:17 +0100

    finished header, main section in progress

--- js/main.js
+++ js/main.js
@@ -10,6 +10,19 @@ const handleNav = () => {
 			nav.classList.remove("nav--active")
 		})
 	})
+
+	handleNavItemsAnimation();
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	})
 }
 
 navBtn.addEventListener("click", handleNav);
+
+
